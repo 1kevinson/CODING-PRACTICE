6d1deca465a8035a9084243f82ab1aaa6c2e9b20
Kevin Kouomeu <kevinkouomeu@icloud.com>
2024-04-28 21:36:25 +0200

    Add synchronised lock

--- Training/Pluralsight/concurrency-and-multithreading/src/main/java/sample/Count.java
+++ Training/Pluralsight/concurrency-and-multithreading/src/main/java/sample/Count.java
@@ -6,15 +6,18 @@ import lombok.extern.slf4j.Slf4j;
 public class Count {
 
     public static int counter = 0;
+    public static final Object lock = new Object();
 
-    static void incrementCounter() {
-        //logger.info("before {} - current thread {}", counter, Thread.currentThread().getId());
-        counter++;
-        logger.info("after {} - current thread {}", counter, Thread.currentThread().getId());
+    private static void incrementCounter() {
+        synchronized (lock) {
+            logger.info("before {} - current thread {}", counter, Thread.currentThread().getId());
+            counter++;
+            logger.info("after {} - current thread {}", counter, Thread.currentThread().getId());
+        }
     }
 
     public static void main(String[] args) {
-        while (counter < 9) {
+        for (int i = 0; i < 10; i++) {
             Thread t1 = new Thread(Count::incrementCounter);
             t1.start();
         }
