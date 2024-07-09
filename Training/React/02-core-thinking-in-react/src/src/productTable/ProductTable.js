ff1fe7a23e596f66c10eadc33b816083756ac255
Kevin Kouomeu <kevinkouomeu@icloud.com>
2024-07-09 08:52:43 +0200

    🔍 Improve product filtering functionality and add lodash
    The product filtering function in 'ProductTable.js' has been enhanced to work more effectively when the filterText is not an empty string. Additionally, the 'lodash' library has been added to the project dependencies in 'package.json' and 'package-lock.json'.

--- Training/React/02-core-thinking-in-react/src/src/productTable/ProductTable.js
+++ Training/React/02-core-thinking-in-react/src/src/productTable/ProductTable.js
@@ -1,50 +1,52 @@
 import { Table } from "antd";
-import './productTable.css'
+import './productTable.css';
 
 export default function ProductTable({ products, filterText, inStockOnly }) {
-    const rows = [];
+  const filtered = [];
 
-    products.forEach((product) => {
-            if (product.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1) return;
-            if (inStockOnly && !product.stocked) return;
-            rows.push(product)
-        }
-    )
+  products.forEach((product) => {
+       if (filterText !== '' && !product.name.toLowerCase().includes(filterText.toLowerCase())) return;
 
-    const columns = [
-        {
-            title: 'Name',
-            key: 'name',
-            dataIndex: 'name',
-            defaultSortOrder: 'ascending',
-            sorter: (a, b) => b.name.toLowerCase().localeCompare(a.name.toLowerCase())
-        },
-        {
-            title: 'Price',
-            key: 'price',
-            dataIndex: 'price',
-            sorter: (a, b) => a.price - b.price,
-        },
-        {
-            title: 'Category',
-            key: 'category',
-            dataIndex: 'category'
-        },
-        {
-            title: 'Stocked',
-            key: 'stocked',
-            render: (text, record) => (`${record.stocked}`)
-        }
-    ];
+       if (inStockOnly && !product.stocked) return;
 
-    return (
-        <Table
-            className='product-table'
-            columns={columns}
-            dataSource={rows}
-            showSorterTooltip={{
-                target: 'sorter-icon',
-            }}
-        />
-    )
+       filtered.push(product)
+     }
+  )
+
+  const columns = [
+    {
+      title: 'Name',
+      key: 'name',
+      dataIndex: 'name',
+      defaultSortOrder: 'ascending',
+      sorter: (a, b) => b.name.toLowerCase().localeCompare(a.name.toLowerCase())
+    },
+    {
+      title: 'Price',
+      key: 'price',
+      dataIndex: 'price',
+      sorter: (a, b) => a.price - b.price,
+    },
+    {
+      title: 'Category',
+      key: 'category',
+      dataIndex: 'category'
+    },
+    {
+      title: 'Stocked',
+      key: 'stocked',
+      render: (text, record) => (`${record.stocked}`)
+    }
+  ];
+
+  return (
+     <Table
+        className='product-table'
+        columns={columns}
+        dataSource={filtered}
+        showSorterTooltip={{
+          target: 'sorter-icon',
+        }}
+     />
+  )
 }
